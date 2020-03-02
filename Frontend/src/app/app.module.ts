c9ca5eab3749c8298ec52fd73a9fb6b7a5b4e2f9
anish210 <61536321+anish210@users.noreply.github.com>
2020-03-02 02:12:00 -0500

    final registeration form

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import{FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { HeaderComponent } from './header/header.component';
-// //import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 // import { PfForm1Component } from './pf-form1/pf-form1.component';
 // import { PfForm2Component } from './pf-form2/pf-form2.component';
 // import { EpfFormComponent } from './epf-form/epf-form.component';
@@ -15,8 +15,8 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    // HeaderComponent,
-    // //FooterComponent,
+     HeaderComponent,
+     FooterComponent,
     // PfForm1Component,
     // PfForm2Component,
     // EpfFormComponent,
