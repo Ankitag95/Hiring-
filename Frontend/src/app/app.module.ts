628fbbfc16e4c4d803d794c804b8c546bd6a513f
gandhi123sagar <sagargandhi193@gmail.com>
2020-03-03 18:25:10 +0530

    Dynamic data(page.component)
    Dynamic Data

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { FooterComponent } from './footer/footer.component';
 import { RegisterComponent } from './register/register.component';
 import{HttpClientModule} from '@angular/common/http';
 import { PageComponent } from './page/page.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,9 @@ import { PageComponent } from './page/page.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgMultiSelectDropDownModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/page-class.spec.ts
@@ -0,0 +1,7 @@
+import { PageClass } from './page-class';
+
+describe('PageClass', () => {
+  it('should create an instance', () => {
+    expect(new PageClass()).toBeTruthy();
+  });
+});
